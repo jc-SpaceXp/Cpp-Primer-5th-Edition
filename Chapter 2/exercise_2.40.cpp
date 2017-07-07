aa033807fb8000e963fb316a531946f6d52d0491
jc-SpaceXp <jamal_clarke@hotmail.co.uk>
2017-07-07 09:48:54 +0100

    updated 2.40.cpp

--- Chapter 2/exercise_2.40.cpp
+++ Chapter 2/exercise_2.40.cpp
@@ -1,7 +1,6 @@
 //EXERCISE 2.40 via C++ Primer 5th Ed.
 #include <iostream>
 
-
 struct Sales_Data {	
 	std::string bookNo;
 	std::string bookName;
@@ -10,10 +9,6 @@ struct Sales_Data {
 	double price = 0.0;
 }
 
-int main()
-{
+int main() {
 	return 0;
 }
-
-
-//EXERCISE 2.40 via C++ Primer 5th Ed.
